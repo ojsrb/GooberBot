3f881ae352d1c7ae41c6ffd490ddd71b7493a7cd
Owen <owenschmidt2010@gmail.com>
2024-11-24 20:01:04 -0600

    new stuff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import random
 import discord
 from discord import Intents
 from discord.ext import commands
@@ -29,10 +29,44 @@ async def ping(interaction: discord.Interaction):
 
 @client.tree.command(name='insult')
 async def insult(interaction: discord.Interaction, user: discord.User):
+    insults = [" is a big meanie!",
+               " is terrible",
+               " is not cool",
+               " is very bad",
+               " is stupid",
+               " is not funny",
+               " is smelly"
+               ]
     if user.id == 800898653820551168:
         await interaction.response.send_message("You cannot insult my papa!", ephemeral=True)
     else:
-        await interaction.channel.send(f"{user.id} is a big meanie!")
+        await interaction.response.send_message("Sending insults...", ephemeral=True)
+        await interaction.channel.send(f"{user.name}{random.choice(insults)}")
+
+@client.tree.command(name='praise')
+async def insult(interaction: discord.Interaction, user: discord.User):
+    praises = [" is very kind!",
+               " is awesome!",
+               " is my favorite member.",
+               " is mega cool :)",
+               " should be president.",
+               " is the GOAT",
+               " is the most amazing person in the world",
+               " is very cool",
+               " is utterly amazing"
+               ]
+
+    await interaction.response.send_message(f"{user}{random.choice(praises)}")
+
+
+@client.tree.command(name='spamzach')
+async def wabbit(interaction: discord.Interaction, times: int):
+    zach = await client.fetch_user(1252305316885041214)
+    dm = await zach.create_dm()
+    await interaction.response.send_message("Spammed Zach", ephemeral=True)
+    for i in range(0, times):
+        await dm.send("stoopid")
+
 
 
 @client.tree.command(name='papa')
@@ -50,5 +84,13 @@ async def say(interaction: discord.Interaction, message: str):
 
     else:
         await interaction.response.send_message("You cant do that!", ephemeral=True)
+
+@client.tree.command(name='dice')
+async def dice(interaction: discord.Interaction, sides: int):
+    await interaction.response.send_message(f"{random.randint(1, sides)}")
+
+@client.tree.command(name='family')
+async def family(interaction: discord.Interaction):
+    await interaction.response.send_message("My papa is o.n., my big bro is Goober")
 
 client.run(TOKEN)
